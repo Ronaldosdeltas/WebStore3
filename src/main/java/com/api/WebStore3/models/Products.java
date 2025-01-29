fbf9060eaf3266a3a60092a17e6709f5fe5801bd
ronaldoss8 <ronaldosssantos1@gmail.com>
2025-01-28 22:20:30 -0300

    creatin the table and the first routes

---
+++ src/main/java/com/api/WebStore3/models/Products.java
@@ -0,0 +1,26 @@
+package com.api.WebStore3.models;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "products")
+public class Products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int id;
+
+    private String name;
+    private String brand;
+    private String category;
+    private double price;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Date createdAt;
+    private String imageFileName;
+
+
+}
