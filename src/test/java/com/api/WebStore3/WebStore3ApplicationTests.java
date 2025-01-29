35e5a98f089aa3ebad70b6e85b51e78372518c7f
ronaldoss8 <ronaldosssantos1@gmail.com>
2025-01-28 21:16:57 -0300

    Initial commit

---
+++ src/test/java/com/api/WebStore3/WebStore3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.WebStore3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStore3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
