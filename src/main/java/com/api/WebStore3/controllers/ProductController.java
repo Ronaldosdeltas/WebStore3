5ceac1041b40c7ff8d065be158a3f3b7e9df09ec
ronaldoss8 <ronaldosssantos1@gmail.com>
2025-02-03 23:55:14 -0300

    finishin the page html creatin prosucts

--- src/main/java/com/api/WebStore3/controllers/ProductController.java
+++ src/main/java/com/api/WebStore3/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.api.WebStore3.controllers;
 import com.api.WebStore3.service.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +13,10 @@ public class ProductController {
 
     @Autowired
     private ProductRepository repo;
+
+    @GetMapping({"", "/"})
+    public String listProducts(Model model) {
+        model.addAttribute("products", repo.findAll());
+        return "products/index";
+    }
 }
